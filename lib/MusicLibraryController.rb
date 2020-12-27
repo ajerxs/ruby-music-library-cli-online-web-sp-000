4176d238dd7a44c8563caf3b7dd3b87e81fee25d
Andrew Eames <ajerxs@gmail.com>
2020-12-27 00:35:30 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -80,6 +80,7 @@ class MusicLibraryController
     song_list = Song.all.sort{|a, b| a.name <=> b.name}
     input = gets.strip.to_i
     song = song_list[input - 1]
+    binding.pry
     puts "Playing #{song.name} by #{song.artist.name}" unless !(1..song_list.size).include?(input - 1)
   end
 
